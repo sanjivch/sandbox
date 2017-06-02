54b3614f5038c908d29e909d12fba09d720b438c
Sanjiv <sanjivch@users.noreply.github.com>
2017-06-02 10:20:34 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -58,9 +58,14 @@ class valve : public pipe{
 class tank{
   public:
     double diaInner, length, volume;
+    double opPressure;
     
     double getVolume(double di, double len){
       return PI*di*di*len*0.25;
+    }
+    
+    double massAccumulated(double dt, double fin, double fout, double h){
+      return h+(dt*(fin-fout))/(1000.0*1.5);
     }
   
 };
@@ -70,33 +75,46 @@ int main()
 {
   //cout << "hi\n";
   srand(time(NULL));
+  double deltaT = 0.1;
+  double flowIn, flowOut, tankLevel =0.0, tankPressure = 101.325;
+  
   //Object definitions
   boundary b1, b2;
-  valve v1;
-  PID pid1;
+  valve v1, v2;
+  PID pid1, pid2;
   tank t1;
   
   //Object inputs
   v1.dInner = 10.0;
   v1.dOuter = 11.0;
   v1.pipeLen = 2.0;
-  v1.Cvmax = 100;
+  v1.Cvmax = 50;
   pid1.CO = 0.5;
+  
+  v2. Cvmax =25.3;
+  pid2.CO = 0.75;
+  
   t1.diaInner = 2.1;
   t1.length = 5.0;
   
   t1.volume = t1.getVolume(t1.diaInner, t1.length);
+  t1.opPressure = 101.325;
   b1.pressureIn = 120.1; //in kPa
   b2.pressureOut = 101.325;
   
   
   
+  
+  
   //Object linking and connections
-  //for(int i=0;i<1000;i++){
-    cout << v1.getflowIn(b1.pressureIn, b2.pressureOut, pid1.CO) << endl;
-    cout << t1.volume;
+  for(double t=0.0;t<100.0;t+=deltaT){
+    flowIn = v1.getflowIn(b1.pressureIn, t1.opPressure, pid1.CO);
+    flowOut = v2.getflowIn(tankPressure, b2.pressureOut, pid2.CO);
+    tankLevel = t1.massAccumulated(deltaT, flowIn, flowOut, tankLevel);
+    tankPressure = 101.325 + (1000*9.80665*tankLevel*0.001);
+    cout << flowIn << " " << flowOut<< " "<< tankLevel <<" " << tankPressure<< endl;
     
-  //}
+  }
   
   
 }
