c05d9f6bc208f0c6d71dd2fbf54270c47bf3f585
Sanjiv <sanjivch@users.noreply.github.com>
2017-08-21 15:38:19 +0530

    Update pfnetnew2.cpp
    Added flow equations

--- pfnetnew2.cpp
+++ pfnetnew2.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 #define N 3
+#define PI 3.1415
 
 class PID{
   public:
@@ -152,23 +153,41 @@ void display(T A[N][N])
     }
 }
 
+double fric(double Re, double dia, double eps){
+  if(Re > 2100.0)
+      return 1.325/pow(log(eps/(3.7*dia) + 5.74/pow(Re,0.9)),2);
+  else
+      return 16.0/Re;
+}
 
 
 int main(){
   
   PID pid1;
   pid1.CO = 0.5;
-  pid1.SP = 1.0;
-  pid1.Kc = -0.15;
-  pid1.Ti = 1.0;
+  pid1.SP = 8.0;
+  pid1.Kc = -0.65;
+  pid1.Ti = 0.20;
   
   double intError2 = 0.0;
   double deltaT = 0.1;
   
   double m1, m2, m3, m4 = 0.0, m5;
-  double nP1 = 101.325, nP2 = 101.325, nP3 = 101.325, soP = 115.0, si1P = 103.0, si2P = 102.3;
+  double l1 = 20.2, l2 = 20.2, l3 = 25.0, l4 = 21.5, l5 = 20.0;
+  double vel1, vel2, vel3, vel4, vel5;
+  double NRe1, NRe2, NRe3, NRe4, NRe5;
+  double epsilon = 0.000061;
+  double f1, f2, f3, f4, f5;
+  double diaIn1= 380.0, diaIn2= 350.0, diaIn3 = 150.0, diaIn4 = 100.0, diaIn5 = 150.0;
+  double kFit1 = 10, kFit2 = 8.5, kFit3 = 11, kFit4 = 20, kFit5 = 11;
+  double area1, area2, area3, area4, area5;
+  double nP1 = 101.325, nP2 = 101.325, nP3 = 101.325, soP = 205.0, si1P = 108.0, si2P = 122.3;
   double dP1 = 0.0, dP2 = 0.0, dP3 = 0.0;
   double Jn[3][3], delP[3][1], res[3][1], invJ[3][3];
+  
+  double rho = 997.3, viscosity = 0.000891;
+  
+  double open;
   
   int iter, i, j;
   /*
@@ -178,47 +197,78 @@ int main(){
                                              =========|m4|=======>si2P
   
   */
-  for(double t=0.0;t<2200.0;t+=deltaT){
-  for(iter=1; iter <25;iter++){
   
-    m1 = sqrt(abs(soP - nP1));
-    m2 = sqrt(abs(nP1 - nP2));
-    m3 = 0.5*sqrt(abs(nP2 - nP3));
-    m4 = pid1.controlPID(deltaT,m4, pid1.SP, intError2)*sqrt(abs(nP2 - si2P));
-    m5 = 0.5*sqrt(abs(nP3 - si1P));
-    
-    
-    Jn[0][0] = m1 + m2;
-    Jn[0][1] = -m2;
-    Jn[0][2] = 0.0;
-    Jn[1][0] = -m2;
-    Jn[1][1] = m2 + m3 + m4;
-    Jn[1][2] = -m3;
-    Jn[2][0] = 0.0;
-    Jn[2][1] = -m3;
-    Jn[2][2] = m3 + m5;
-    
-    res[0][0] = m1 - m2;
-    res[1][0] = m2 - m3 - m4;
-    res[2][0] = m3 - m5;
-    
-    if (inverse(Jn, invJ))
-         // display(invJ);
-          
-    
-    delP[0][0] = invJ[0][0] * res[0][0] + invJ[0][1] * res[1][0] + invJ[0][2] * res[2][0];
-    delP[1][0] = invJ[1][0] * res[0][0] + invJ[1][1] * res[1][0] + invJ[1][2] * res[2][0];
-    delP[2][0] = invJ[2][0] * res[0][0] + invJ[2][1] * res[2][0] + invJ[2][2] * res[2][0];
+  area1 = 0.25*PI*pow(0.001*diaIn1,2);
+  area2 = 0.25*PI*pow(0.001*diaIn2,2);
+  area3 = 0.25*PI*pow(0.001*diaIn3,2);
+  area4 = 0.25*PI*pow(0.001*diaIn4,2);
+  area5 = 0.25*PI*pow(0.001*diaIn5,2);
+  
+  NRe1 = NRe2 = NRe3 = NRe4 = NRe5 = 2100.0;
+  
+  for(double t=0.0;t<2500.0;t+=deltaT){
+    f1 = fric(NRe1, diaIn1, epsilon);
+    f2 = fric(NRe2, diaIn2, epsilon);
+    f3 = fric(NRe3, diaIn3, epsilon);
+    f4 = fric(NRe4, diaIn4, epsilon);
+    f5 = fric(NRe5, diaIn5, epsilon);
+  
+  
+    for(iter=1; iter <25;iter++){
+     
+      m1 = area1 * sqrt(2 * rho * abs(soP - nP1)/(f1*l1*1000.0/diaIn1 + kFit1));
+      m2 = area2 * sqrt(2 * rho * abs(nP1 - nP2)/(f2*l2*1000.0/diaIn2 + kFit2));
+      m3 = area3 * sqrt(2 * rho * abs(nP2 - nP3)/(f3*l3*1000.0/diaIn3 + kFit3));
+      open = pid1.controlPID(deltaT,m4, pid1.SP, intError2);
+      m4 = open * area4 * sqrt(2 * rho * abs(nP2 - si2P)/(f4*l4*1000.0/diaIn4 + kFit4));
+      m5 = area5 * sqrt(2 * rho * abs(nP3 - si1P)/(f5*l5*1000.0/diaIn5 + kFit5));
       
-    nP1 = nP1 + delP[0][0];
-    nP2 = nP2 + delP[1][0];
-    nP3 = nP3 + delP[2][0];
+      
+      Jn[0][0] = m1 + m2;
+      Jn[0][1] = -m2;
+      Jn[0][2] = 0.0;
+      Jn[1][0] = -m2;
+      Jn[1][1] = m2 + m3 + m4;
+      Jn[1][2] = -m3;
+      Jn[2][0] = 0.0;
+      Jn[2][1] = -m3;
+      Jn[2][2] = m3 + m5;
+      
+      res[0][0] = m1 - m2;
+      res[1][0] = m2 - m3 - m4;
+      res[2][0] = m3 - m5;
+      
+      if (inverse(Jn, invJ))
+           // display(invJ);
+            
+      
+      delP[0][0] = invJ[0][0] * res[0][0] + invJ[0][1] * res[1][0] + invJ[0][2] * res[2][0];
+      delP[1][0] = invJ[1][0] * res[0][0] + invJ[1][1] * res[1][0] + invJ[1][2] * res[2][0];
+      delP[2][0] = invJ[2][0] * res[0][0] + invJ[2][1] * res[2][0] + invJ[2][2] * res[2][0];
+        
+      nP1 = nP1 + delP[0][0];
+      nP2 = nP2 + delP[1][0];
+      nP3 = nP3 + delP[2][0];
+      
+     // cout << m1 << " " << m2 << " " << m3 << " " << m4 << " " << nP1 << " " << nP2 << " " << delP[0][0]<<" "<<delP[1][0]<< endl;
+    }
     
-   // cout << m1 << " " << m2 << " " << m3 << " " << m4 << " " << nP1 << " " << nP2 << " " << delP[0][0]<<" "<<delP[1][0]<< endl;
-  }
+      vel1 = m1/(rho * area1);
+      vel2 = m2/(rho * area2);
+      vel3 = m3/(rho * area3);
+      vel4 = m4/(rho * area4);
+      vel5 = m5/(rho * area5);
+      
+      NRe1 = 0.001*diaIn1 * vel1 * rho/ viscosity;
+      NRe2 = 0.001*diaIn2 * vel2 * rho/ viscosity;
+      NRe3 = 0.001*diaIn3 * vel3 * rho/ viscosity;
+      NRe4 = 0.001*diaIn4 * vel4 * rho/ viscosity;
+      NRe5 = 0.001*diaIn5 * vel5 * rho/ viscosity;
   
   intError2 = pid1.errorIntegral(deltaT, m4, pid1.SP, intError2);
-  
-  cout << m1 << " " << m2 << " " << m3 << " " <<m5<< " " << m4 << " " << nP1 << " " << nP2 << " " <<nP3 <<" "<< delP[0][0]<<" "<<delP[1][0]<< " " << delP[2][0] << " "<<endl;
+  cout << m4 << " " << open<<endl;
+  //cout << m1 << " " << m2 << " " << m3 << " " <<m5<< " " << m4 << " " << nP1 << " " << nP2 << " " <<nP3 <<" "<< delP[0][0]<<" "<<delP[1][0]<< " " << delP[2][0] << " "<<endl;
   }
+  
+  cout << m1 << " " << m2 << " " << m3 << " " <<m5<< " " << m4 << " " << nP1 << " " << nP2 << " " <<nP3 <<" "<< delP[0][0]<<" "<<delP[1][0]<< " " << delP[2][0] << " "<<open <<endl;
 }
